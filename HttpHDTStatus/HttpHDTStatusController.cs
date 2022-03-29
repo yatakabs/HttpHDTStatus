3cb5a973ce5d4e54ead0333d3322da73fae381ff
デンパ時計 <jsoldenpa@yahoo.co.jp>
2022-03-29 23:42:14 +0900

    プロジェクト ファイルを追加します。

---
+++ HttpHDTStatus/HttpHDTStatusController.cs
@@ -0,0 +1,81 @@
+﻿using HeadDistanceTravelled;
+using HttpSiraStatus;
+using HttpSiraStatus.Interfaces;
+using HttpSiraStatus.Util;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Zenject;
+
+namespace HttpHDTStatus
+{
+    public class HttpHDTStatusController : IInitializable, IDisposable
+    {
+        //ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*
+        #region // パブリックメソッド
+        public void Initialize()
+        {
+            this._controller.OnDistanceChanged += this.OnDistanceChanged;
+        }
+        #endregion
+        //ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*
+        #region // プライベートメソッド
+        private void OnDistanceChanged(float distance, in Vector3 hmdPosition, in Quaternion hmdRotation)
+        {
+            var posJson = new JSONObject();
+            posJson["x"] = hmdPosition.x;
+            posJson["y"] = hmdPosition.y;
+            posJson["z"] = hmdPosition.z;
+            var rotationJson = new JSONObject();
+            var eula = hmdRotation.eulerAngles;
+            rotationJson["x"] = eula.x;
+            rotationJson["y"] = eula.y;
+            rotationJson["z"] = eula.z;
+            var rootObj = new JSONObject();
+            rootObj["Position"] = posJson;
+            rootObj["Rotation"] = rotationJson;
+            rootObj["Distance"] = distance;
+            this._statusManager.OtherJSON["HeadDistanceTravelled"] = rootObj;
+
+            this._statusManager.EmitStatusUpdate(ChangedProperty.Other, BeatSaberEvent.Other);
+        }
+        #endregion
+        //ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*
+        #region // メンバ変数
+        private bool _disposedValue;
+        private IHeadDistanceTravelledController _controller;
+        private IStatusManager _statusManager;
+        #endregion
+        //ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*
+        #region // 構築・破棄
+        [Inject]
+        public HttpHDTStatusController(IHeadDistanceTravelledController controller, IStatusManager statusManager)
+        {
+            this._controller = controller;
+            this._statusManager = statusManager;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposedValue) {
+                if (disposing) {
+                    this._controller.OnDistanceChanged -= this.OnDistanceChanged;
+                }
+                _disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            // このコードを変更しないでください。クリーンアップ コードを 'Dispose(bool disposing)' メソッドに記述します
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
